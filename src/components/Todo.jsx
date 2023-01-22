dfc97a05b45b85d8d6f8f8b3850a06d937501519
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-22 22:51:12 +0330

    create todo component for every single todo of project

---
+++ src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+const Todo = ({ todos }) => {
+  return (
+    <div>
+      <ul className="">
+        {todos.map((todo) => {
+          return (
+            <li className="flex  justify-between bg-slate-200 p-4 my-2 capitalize ">
+              <div className="flex ">
+                <input type="checkbox" />
+                <p className="ml-2 cursor-pointer">{todo}</p>
+              </div>
+
+              <button className="cursor-pointer flex items-center">
+                <RiDeleteBin5Line />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+     
+    </div>
+  );
+};
+
+export default Todo;
