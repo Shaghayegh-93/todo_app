0531b39f020ed2c2bd445eca7829580b242551b6
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-24 03:32:46 +0330

    add my information of database of project to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyAuUDfxVTVb5Bl-thQRSSJtFw2O0l7zAS4",
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "todo-app-ce17f.firebaseapp.com",
-  projectId: "todo-app-ce17f",
-  storageBucket: "todo-app-ce17f.appspot.com",
-  messagingSenderId: "382929657011",
-  appId: "1:382929657011:web:d0afa16b05adf7ea5d64c6",
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APPID,
 };
-// console.log(import.meta.env.VITE_API_KEY);
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
