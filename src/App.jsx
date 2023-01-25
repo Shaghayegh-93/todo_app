ac4b17fad1dee3c77c721861110583c3ecd9aca1
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-25 04:08:52 +0330

    add a function and send as a props to Todo component for toggling todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { AiOutlinePlus } from "react-icons/ai";
 import Todo from "./components/Todo";
 import { useState } from "react";
 import { useEffect } from "react";
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { collection, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  // Read Data
   useEffect(() => {
     const q = query(collection(db, "todos"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -19,6 +20,13 @@ function App() {
     });
     return () => unsubscribe();
   }, []);
+  // update data
+  const toggleTodoComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
   return (
     <div className="w-screen h-screen p-4 bg-gradient-to-b from-[#c4b5fd] to-[#5b21b6] ">
       <div className=" w-full max-w-[500px]   m-auto bg-slate-100 p-4 shadow-xl rounded-md ">
@@ -35,7 +43,7 @@ function App() {
             <AiOutlinePlus size={30} />
           </button>
         </form>
-        <Todo todos={todos} />
+        <Todo todos={todos} toggleTodoComplete={toggleTodoComplete} />
         <p className="text-center p-2">You have {todos.length} todos</p>
       </div>
     </div>
